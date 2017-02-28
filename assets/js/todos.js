2b12ecf2bb811ce282ab76a8993ba6ba099c0574
elijahchen <code.elijahchen@gmail.com>
2017-02-28 14:04:51 -0500

    - Added listener on the (li span), upon clicking on the "O" it will remove the corresponding li

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -5,3 +5,11 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
+
+//Click on X to delete todos
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation(); //Stops order bubbling
+});
