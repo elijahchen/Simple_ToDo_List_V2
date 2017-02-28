2777548eb9efc97576c6601bc933d59ac527629f
elijahchen <code.elijahchen@gmail.com>
2017-02-28 00:08:33 -0500

    - Added additional basic formatting

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,15 @@
 /**
  * Created by Elijah on 2/27/2017.
  */
+//Check off specific todos by clicking
+$("li").click(function(){
+    console.log($(this).css("color"));
+    // if($(this).css("color") == "rgb(128, 128, 128)"){
+    //     $(this).css("color", "black");
+    // }
+
+    $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    });
+});
