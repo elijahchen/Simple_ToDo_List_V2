2bf3deb2ec939464792fbed29c83ec67c4e05cba
elijahchen <code.elijahchen@gmail.com>
2017-02-28 14:36:04 -0500

    - Added listener to read val() from input and appended as a new todo on the list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,14 +2,24 @@
  * Created by Elijah on 2/27/2017.
  */
 //Check off specific todos by clicking
-$("li").click(function(){
+$("li").click(function () {
     $(this).toggleClass("completed");
 });
 
 //Click on X to delete todos
-$("span").click(function(event){
+$("span").click(function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation(); //Stops order bubbling
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) { //If enter key is fired
+        //Storing values from input
+        var todoText = $(this).val();
+        $(this).val(""); //Sets new value of an empty string
+        //Create new li and add to parent ul
+        $("ul").append("<li><span>O</span> " + todoText + "</li>");
+    }
 });
